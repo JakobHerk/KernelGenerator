0bee0a9168a658f271014bf635317f8879293f07
Jakob Herk <13jakob11@gmail.com>
2019-01-08 12:39:06 +0100

    because sebastian had some difficulties

--- KernelGenerator/src/main.cpp
+++ KernelGenerator/src/main.cpp
@@ -14,6 +14,10 @@ int setupLoggerCpp(po::variables_map &vm);
 int setupConfigdataCpp(po::variables_map &vm);
 int setupShmHandlerCpp(po::variables_map &vm);
 int setupRasppidataCpp(po::variables_map &vm);
+int setupLoggerH(po::variables_map &vm);
+int setupConfigdataH(po::variables_map &vm);
+int setupShmHandlerH(po::variables_map &vm);
+int setupRasppidataH(po::variables_map &vm);
 
 int main(int argc, const char *argv[])
 {
@@ -21,7 +25,6 @@ int main(int argc, const char *argv[])
     {
         po::options_description desc{"Options"};
         desc.add_options()
-                //TODO add list of all available options
                 ("help,h", "")
                 ("projectname,p", po::value<std::string>()->default_value("Kernel"), "Projectname")
                 ("shmname,n", po::value<std::string>()->default_value("theSHM"), "Shmname")
@@ -64,8 +67,143 @@ int setupProjectEnvironment(std::string projectname){
 }
 
 //Sebastian Pack
-int setupHeaderfiles(){
+int setupHeaderfiles(po::variables_map &vm){
+    setupLoggerH(vm);
+    setupConfigdataH(vm);
+    setupRasppidataH(vm);
+    setupShmHandlerH(vm);
+}
 
+int setupLoggerH(po::variables_map &vm)
+{
+    std::stringstream text;
+    text << "#pragma once"<< std::endl <<
+            "#include <boost/thread/mutex.hpp>" << std::endl <<
+            "#include <boost/log/trivial.hpp>"<< std::endl <<
+            "#include <iomanip>"<< std::endl <<
+            "#include <string>"<< std::endl <<
+            "class Logger"<< std::endl <<
+            "{"<< std::endl <<
+            "public:"<< std::endl <<
+            "  static Logger* Instance();"<< std::endl <<
+            "  void initLogger(unsigned int rotationSizeInMB, boost::log::trivial::severity_level severityLevel, std::string directory, std::string file);"<< std::endl <<
+            "  void logInfo(std::string message);"<< std::endl <<
+            "  void logDebug(std::string message);"<< std::endl <<
+            "  void logWarn(std::string message);"<< std::endl <<
+            "  void logError(std::string message);"<< std::endl <<
+            "  void logFatal(std::string message);"<< std::endl <<
+            "private:"<< std::endl <<
+            "  void createOrReplaceDirectory(std::string directory);"<< std::endl <<
+            "  static Logger* _instance;"<< std::endl <<
+            "  boost::mutex _mutex;"<< std::endl <<
+            "  boost::log::sources::severity_logger<boost::log::trivial::severity_level> _log;"<< std::endl <<
+            "protected:"<< std::endl <<
+            "  Logger();"<< std::endl <<
+            "};";
+
+    std::ofstream out;
+    std::string path;
+    path.append(vm["projectpath"].as<std::string>());
+    path.append("/incl/logger.h");
+
+    out.open(path);
+    out << text.str();
+    out.flush();
+    out.close();
+}
+
+int setupConfigdataH(po::variables_map &vm)
+{
+    std::stringstream text;
+    text << "#ifndef CONFIGDATA_H" << std::endl <<
+            "#define CONFIGDATA_H" << std::endl <<
+            "#include <string.h>" << std::endl <<
+            "#include <iostream>" << std::endl <<
+            "#include <fstream>" << std::endl <<
+            "#include <map>" << std::endl <<
+            "#include <boost/algorithm/string/predicate.hpp>" << std::endl <<
+            "#include <boost/lexical_cast.hpp>" << std::endl <<
+            "using namespace std;" << std::endl <<
+            "class ConfigData" << std::endl <<
+            "{" << std::endl <<
+            "public:" << std::endl <<
+            "    ConfigData(string path);" << std::endl <<
+            "    ~ConfigData();" << std::endl <<
+            "    void setConfigObject();" << std::endl <<
+            "    map<string,string> returnProcessMap();" << std::endl <<
+            "    void addItemToProcessMap(string key,string value);" << std::endl <<
+            "    string trim(string& str);" << std::endl <<
+            "private:" << std::endl <<
+            "    string m_path;" << std::endl <<
+            "    map<string,string> m_ProcessMap;" << std::endl <<
+            "};" << std::endl <<
+            "#endif // CONFIGDATA_H";
+
+    std::ofstream out;
+    std::string path;
+    path.append(vm["projectpath"].as<std::string>());
+    path.append("/incl/configdata.h");
+
+    out.open(path);
+    out << text.str();
+    out.flush();
+    out.close();
+}
+
+int setupRasppidataH(po::variables_map &vm)
+{
+    std::stringstream text;
+    text << "#ifndef RASPPIDATA_H" << std::endl <<
+            "#define RASPPIDATA_H" << std::endl <<
+            "class RaspPiData" << std::endl <<
+            "{" << std::endl <<
+            "public:" << std::endl <<
+            "    RaspPiData();" << std::endl <<
+            "};" << std::endl <<
+            "#endif // RASPPIDATA_H";
+
+    std::ofstream out;
+    std::string path;
+    path.append(vm["projectpath"].as<std::string>());
+    path.append("/incl/rasppidata.h");
+
+    out.open(path);
+    out << text.str();
+    out.flush();
+    out.close();
+}
+
+int setupShmHandlerH(po::variables_map &vm)
+{
+    std::stringstream text;
+    text <<"#ifndef SIMPLESHAREDMEMORYHANDLER_H" <<std::endl <<
+           "#define SIMPLESHAREDMEMORYHANDLER_H" << std::endl <<
+           "#include <boost/interprocess/managed_shared_memory.hpp>" << std::endl <<
+           "#include <string>" << std::endl <<
+           "#include <iostream>" << std::endl <<
+           "#include \"rasppidata.h\"" << std::endl <<
+           "class SimpleSharedMemoryHandler" << std::endl <<
+           "{" << std::endl <<
+           "public:" << std::endl <<
+           "    SimpleSharedMemoryHandler(std::string sharedMemoryName, int size);" << std::endl <<
+           "    ~SimpleSharedMemoryHandler();" << std::endl <<
+           "    void setData(std::string varName, std::__cxx11::string data);" << std::endl <<
+           "private:" << std::endl <<
+           "    boost::interprocess::managed_shared_memory* mySharedMemory;" << std::endl <<
+           "    std::string sharedMemoryName, raspPiname;" << std::endl <<
+           "};" << std::endl <<
+           "#endif // SIMPLESHAREDMEMORYHANDLER_H";
+
+
+    std::ofstream out;
+    std::string path;
+    path.append(vm["projectpath"].as<std::string>());
+    path.append("/incl/simplesharedmemoryhandler.h");
+
+    out.open(path);
+    out << text.str();
+    out.flush();
+    out.close();
 }
 
 //Jakob Herk
